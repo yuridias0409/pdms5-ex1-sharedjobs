19881726cae1d971ac378b828d8974d14489d417
yuridias0409 <62430812+yuridias0409@users.noreply.github.com>
2021-04-20 18:36:05 -0300

    Adição ViewBinding
    Removendo findViewById e adicionando viewBinding

--- app/src/main/java/pdms5/at1/sharedjobs/MainActivity.java
+++ app/src/main/java/pdms5/at1/sharedjobs/MainActivity.java
@@ -15,84 +15,65 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText editTextTextPersonName;
-    private EditText editTextTextEmailAddress;
-    private EditText editTextPhone;
-    private EditText editTextAnoFormatura;
-    private EditText editTextAnoConclusao;
-    private EditText editTextInstituicao;
-    private EditText editTextMonografia;
-    private EditText editTextOrientador;
-    private EditText editTextCell;
-    private EditText editTextDate;
-    private EditText editTextVagasInteresse;
-    private CheckBox checkBoxAttsEmail;
-    private CheckBox checkBoxTelComercial;
-    private RadioGroup radioGroupSexo;
-    private RadioButton radioButtonMasc;
-    private RadioButton radioButtonFem;
-    private RadioButton radioButtonOutro;
-    private Switch switchCell;
-    private Spinner spinnerFormacao;
-    private Button buttonSalvar;
-    private Button buttonLimpar;
+import pdms5.at1.sharedjobs.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         // Ligando (binding) objetos com as Views
-        bindViews();
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
 
         //Seta masculino como selecionado desde o começo
-        radioButtonMasc.setChecked(true);
+        activityMainBinding.radioButtonMasc.setChecked(true);
 
-        switchCell.setOnClickListener(new View.OnClickListener() {
+        activityMainBinding.switchCell.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Celular
-                if(switchCell.isChecked()){
-                    editTextCell.setVisibility(View.VISIBLE);
+                if(activityMainBinding.switchCell.isChecked()){
+                    activityMainBinding.editTextCell.setVisibility(View.VISIBLE);
                 }   else{
-                    editTextCell.setVisibility(View.GONE);
+                    activityMainBinding.editTextCell.setVisibility(View.GONE);
                 }
             }
         });
 
         // Listener de item selecionado
-        spinnerFormacao.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        activityMainBinding.spinnerFormacao.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if (((TextView) view).getText().equals("Fundamental") || ((TextView) view).getText().equals("Ensino Medio")){
                     //Retira os outros campos
-                    editTextAnoConclusao.setVisibility(View.GONE);
-                    editTextAnoFormatura.setVisibility(View.GONE);
-                    editTextMonografia.setVisibility(View.GONE);
-                    editTextOrientador.setVisibility(View.GONE);
+                    activityMainBinding.editTextAnoConclusao.setVisibility(View.GONE);
+                    activityMainBinding.editTextAnoFormatura.setVisibility(View.GONE);
+                    activityMainBinding.editTextMonografia.setVisibility(View.GONE);
+                    activityMainBinding.editTextOrientador.setVisibility(View.GONE);
 
                     //Ativa o campo
-                    editTextAnoFormatura.setVisibility(View.VISIBLE);
+                    activityMainBinding.editTextAnoFormatura.setVisibility(View.VISIBLE);
                 }   else if(((TextView) view).getText().equals("Graduação") || ((TextView) view).getText().equals("Especialização")){
                     //Retira os outros campos
-                    editTextAnoFormatura.setVisibility(View.GONE);
-                    editTextMonografia.setVisibility(View.GONE);
-                    editTextOrientador.setVisibility(View.GONE);
+                    activityMainBinding.editTextAnoFormatura.setVisibility(View.GONE);
+                    activityMainBinding.editTextMonografia.setVisibility(View.GONE);
+                    activityMainBinding.editTextOrientador.setVisibility(View.GONE);
 
                     //Ativa o campo
-                    editTextAnoConclusao.setVisibility(View.VISIBLE);
-                    editTextInstituicao.setVisibility(View.VISIBLE);
+                    activityMainBinding.editTextAnoConclusao.setVisibility(View.VISIBLE);
+                    activityMainBinding.editTextInstituicao.setVisibility(View.VISIBLE);
                 }   else if(((TextView) view).getText().equals("Mestrado") || ((TextView) view).getText().equals("Doutorado")){
                     //Retira os outros campos
-                    editTextAnoFormatura.setVisibility(View.GONE);
+                    activityMainBinding.editTextAnoFormatura.setVisibility(View.GONE);
 
                     //Ativa o campo
-                    editTextAnoConclusao.setVisibility(View.VISIBLE);
-                    editTextInstituicao.setVisibility(View.VISIBLE);
-                    editTextMonografia.setVisibility(View.VISIBLE);
-                    editTextOrientador.setVisibility(View.VISIBLE);
+                    activityMainBinding.editTextAnoConclusao.setVisibility(View.VISIBLE);
+                    activityMainBinding.editTextInstituicao.setVisibility(View.VISIBLE);
+                    activityMainBinding.editTextMonografia.setVisibility(View.VISIBLE);
+                    activityMainBinding.editTextOrientador.setVisibility(View.VISIBLE);
                 }
             }
 
@@ -119,29 +100,29 @@ public class MainActivity extends AppCompatActivity {
 
     private void saveForm() {
         StringBuffer sumarioSb = new StringBuffer();
-        sumarioSb.append("Nome completo: ").append(editTextTextPersonName.getText().toString()).append("\n");
-        sumarioSb.append("E-mail: ").append(editTextTextEmailAddress.getText().toString()).append("\n");
+        sumarioSb.append("Nome completo: ").append(activityMainBinding.editTextTextPersonName.getText().toString()).append("\n");
+        sumarioSb.append("E-mail: ").append(activityMainBinding.editTextTextEmailAddress.getText().toString()).append("\n");
 
-        if(checkBoxAttsEmail.isChecked()){
+        if(activityMainBinding.checkBoxAttsEmail.isChecked()){
             sumarioSb.append("Receber Emails: ").append("true").append("\n");
         }   else{
             sumarioSb.append("Receber Emails: ").append("false").append("\n");
         }
 
-        sumarioSb.append("Telefone: ").append(editTextPhone.getText().toString()).append("\n");
+        sumarioSb.append("Telefone: ").append(activityMainBinding.editTextPhone.getText().toString()).append("\n");
 
-        if(checkBoxTelComercial.isChecked()){
+        if(activityMainBinding.checkBoxTelComercial.isChecked()){
             sumarioSb.append("Telefone comercial: ").append("true").append("\n");
         }   else{
             sumarioSb.append("Telefone comercial: ").append("false").append("\n");
         }
 
-        if(switchCell.isChecked()){
-            sumarioSb.append("Celular: ").append(editTextCell.getText().toString()).append("\n");
+        if(activityMainBinding.switchCell.isChecked()){
+            sumarioSb.append("Celular: ").append(activityMainBinding.editTextCell.getText().toString()).append("\n");
         }
 
         sumarioSb.append("Sexo: ");
-        switch (radioGroupSexo.getCheckedRadioButtonId()) {
+        switch (activityMainBinding.radioGroupSexo.getCheckedRadioButtonId()) {
             case R.id.radioButtonMasc:
                 sumarioSb.append("masculino");
                 break;
@@ -154,68 +135,43 @@ public class MainActivity extends AppCompatActivity {
         }
         sumarioSb.append("\n");
 
-        sumarioSb.append("Data de Nascimento: ").append(editTextDate.getText().toString()).append("\n");
+        sumarioSb.append("Data de Nascimento: ").append(activityMainBinding.editTextDate.getText().toString()).append("\n");
 
-        sumarioSb.append("Formacao: ").append(((TextView) spinnerFormacao.getSelectedView()).getText()).append("\n");
+        sumarioSb.append("Formacao: ").append(((TextView) activityMainBinding.spinnerFormacao.getSelectedView()).getText()).append("\n");
 
-        if (spinnerFormacao.getSelectedItemPosition() == 0 || spinnerFormacao.getSelectedItemPosition() == 1){
-            sumarioSb.append("Ano de Formatura: ").append(editTextAnoFormatura.getText().toString()).append("\n");
-        }else if (spinnerFormacao.getSelectedItemPosition() == 2 || spinnerFormacao.getSelectedItemPosition() == 3){
-            sumarioSb.append("Ano de Conclusão: ").append(editTextAnoConclusao.getText().toString()).append("\n");
-            sumarioSb.append("Instituição: ").append(editTextInstituicao.getText().toString()).append("\n");
-        }else if (spinnerFormacao.getSelectedItemPosition() == 4 || spinnerFormacao.getSelectedItemPosition() == 5){
-            sumarioSb.append("Ano de Conclusão: ").append(editTextAnoConclusao.getText().toString()).append("\n");
-            sumarioSb.append("Instituição: ").append(editTextInstituicao.getText().toString()).append("\n");
-            sumarioSb.append("Título da Monografia: ").append(editTextMonografia.getText().toString()).append("\n");
-            sumarioSb.append("Orientador: ").append(editTextOrientador.getText().toString()).append("\n");
+        if (activityMainBinding.spinnerFormacao.getSelectedItemPosition() == 0 || activityMainBinding.spinnerFormacao.getSelectedItemPosition() == 1){
+            sumarioSb.append("Ano de Formatura: ").append(activityMainBinding.editTextAnoFormatura.getText().toString()).append("\n");
+        }else if (activityMainBinding.spinnerFormacao.getSelectedItemPosition() == 2 || activityMainBinding.spinnerFormacao.getSelectedItemPosition() == 3){
+            sumarioSb.append("Ano de Conclusão: ").append(activityMainBinding.editTextAnoConclusao.getText().toString()).append("\n");
+            sumarioSb.append("Instituição: ").append(activityMainBinding.editTextInstituicao.getText().toString()).append("\n");
+        }else if (activityMainBinding.spinnerFormacao.getSelectedItemPosition() == 4 || activityMainBinding.spinnerFormacao.getSelectedItemPosition() == 5){
+            sumarioSb.append("Ano de Conclusão: ").append(activityMainBinding.editTextAnoConclusao.getText().toString()).append("\n");
+            sumarioSb.append("Instituição: ").append(activityMainBinding.editTextInstituicao.getText().toString()).append("\n");
+            sumarioSb.append("Título da Monografia: ").append(activityMainBinding.editTextMonografia.getText().toString()).append("\n");
+            sumarioSb.append("Orientador: ").append(activityMainBinding.editTextOrientador.getText().toString()).append("\n");
         }
 
-        sumarioSb.append("Vagas de interesse: ").append(editTextVagasInteresse.getText().toString()).append("\n");
+        sumarioSb.append("Vagas de interesse: ").append(activityMainBinding.editTextVagasInteresse.getText().toString()).append("\n");
 
         Toast.makeText(this, sumarioSb.toString(), Toast.LENGTH_SHORT).show();
     }
 
     private void cleanForm() {
-        editTextTextPersonName.setText("");
-        editTextTextEmailAddress.setText("");
-        editTextPhone.setText("");
-        editTextAnoFormatura.setText("");
-        editTextAnoConclusao.setText("");
-        editTextMonografia.setText("");
-        editTextOrientador.setText("");
-        editTextInstituicao.setText("");
-        editTextCell.setText("");
-        editTextDate.setText("");
-        editTextVagasInteresse.setText("");
-        radioButtonMasc.setChecked(true);
-        spinnerFormacao.setSelection(0);
-        checkBoxAttsEmail.setChecked(false);
-        checkBoxTelComercial.setChecked(false);
-        switchCell.setChecked(false);
+        activityMainBinding.editTextTextPersonName.setText("");
+        activityMainBinding.editTextTextEmailAddress.setText("");
+        activityMainBinding.editTextPhone.setText("");
+        activityMainBinding.editTextAnoFormatura.setText("");
+        activityMainBinding.editTextAnoConclusao.setText("");
+        activityMainBinding.editTextMonografia.setText("");
+        activityMainBinding.editTextOrientador.setText("");
+        activityMainBinding.editTextInstituicao.setText("");
+        activityMainBinding.editTextCell.setText("");
+        activityMainBinding.editTextDate.setText("");
+        activityMainBinding.editTextVagasInteresse.setText("");
+        activityMainBinding.radioButtonMasc.setChecked(true);
+        activityMainBinding.spinnerFormacao.setSelection(0);
+        activityMainBinding.checkBoxAttsEmail.setChecked(false);
+        activityMainBinding.checkBoxTelComercial.setChecked(false);
+        activityMainBinding.switchCell.setChecked(false);
     }
-
-    private void bindViews() {
-        editTextTextPersonName = findViewById(R.id.editTextTextPersonName);
-        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
-        editTextPhone = findViewById(R.id.editTextPhone);
-        editTextAnoFormatura = findViewById(R.id.editTextAnoFormatura);
-        editTextAnoConclusao = findViewById(R.id.editTextAnoConclusao);
-        editTextInstituicao = findViewById(R.id.editTextInstituicao);
-        editTextMonografia = findViewById(R.id.editTextMonografia);
-        editTextOrientador = findViewById(R.id.editTextOrientador);
-        editTextCell = findViewById(R.id.editTextCell);
-        editTextDate = findViewById(R.id.editTextDate);
-        editTextVagasInteresse = findViewById(R.id.editTextVagasInteresse);
-        checkBoxAttsEmail = findViewById(R.id.checkBoxAttsEmail);
-        checkBoxTelComercial = findViewById(R.id.checkBoxTelComercial);
-        radioGroupSexo = findViewById(R.id.radioGroupSexo);
-        switchCell = findViewById(R.id.switchCell);
-        spinnerFormacao = findViewById(R.id.spinnerFormacao);
-        radioButtonMasc = findViewById(R.id.radioButtonMasc);
-        radioButtonOutro = findViewById(R.id.radioButtonOutro);
-        radioButtonFem = findViewById(R.id.radioButtonFem);
-        buttonSalvar = findViewById(R.id.buttonSalvar);
-        buttonLimpar = findViewById(R.id.buttonLimpar);
-    }
-
 }
